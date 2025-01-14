57386de697da7dca0bcf18fce4225e7690ac054e
wander <hamzaotu1@gmail.com>
2025-01-14 19:23:19 +0000

    Add project files.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private float speed;
+
+    private readonly int moveX = Animator.StringToHash("MoveX");
+    private readonly int moveY = Animator.StringToHash("MoveY");
+
+    private PlayerActions actions;
+    private Rigidbody2D rb2D;
+    private Animator animator;
+    private Vector2 moveDirection;
+
+    private void Awake()
+    {
+        actions = new PlayerActions();
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb2D.MovePosition(rb2D.position + moveDirection * (speed * Time.fixedDeltaTime));
+    }
+
+    private void ReadMovement()
+    {
+        moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+        if (moveDirection == Vector2.zero) return;
+        animator.SetFloat(moveX, moveDirection.x);
+        animator.SetFloat(moveY, moveDirection.y);
+    }
+
+    private void OnEnable()
+    {
+        actions.Enable();
+    }
+    
+    private void OnDisable()
+    {
+        actions.Disable();
+    }
+}
